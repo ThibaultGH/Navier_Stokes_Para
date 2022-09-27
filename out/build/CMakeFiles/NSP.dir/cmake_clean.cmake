de1a45d0b8c213b9e8e3e1e22b59948de46dc10f
ThibaultGH <thibault.cimic@inria.fr>
2022-09-27 10:13:56 +0200

    Add cmake files and repo.

---
+++ out/build/CMakeFiles/NSP.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NSP.dir/main.cpp.o"
+  "CMakeFiles/NSP.dir/main.cpp.o.d"
+  "NSP"
+  "NSP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NSP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/CMakeFiles/NSP.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for NSP.
+# This may be replaced when dependencies are built.
